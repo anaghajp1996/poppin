061f91e60ac8264d8460acdb4beccd55f23fe47c
Anagha <anaghajayaprakash1996@gmail.com>
2023-12-01 23:02:46 -0600

    Initial Commit

---
+++ poppin/poppinApp.swift
@@ -0,0 +1,17 @@
+//
+//  poppinApp.swift
+//  poppin
+//
+//  Created by Anagha K J on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct poppinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
